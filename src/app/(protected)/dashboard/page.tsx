76236d66e74934ce7e3bf68d8ec1b08c201f7d15
Aman Kumar <aman100170@gmail.com>
2025-01-14 19:31:23 +0530

    Added commit log component

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -2,6 +2,7 @@
 import useProject from "@/hooks/use-project";
 import { ExternalLink, Github } from "lucide-react";
 import Link from "next/link";
+import CommitLog from "./commit-log";
 
 const DashboardPage = () => {
     const { project } = useProject();
@@ -43,7 +44,7 @@ const DashboardPage = () => {
             </div>
 
             <div className="mt-8"></div>
-            CommitLog
+            <CommitLog />
 
         </div>
     )

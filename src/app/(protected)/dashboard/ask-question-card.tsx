c394ad44fd93d737f5255d9ba5eb2f177ab9c560
Aman Kumar <aman100170@gmail.com>
2025-01-15 21:07:55 +0530

    Added Q&A Page Component

--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import MDEditor from '@uiw/react-md-editor';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Textarea } from "@/components/ui/textarea";
 import useProject from "@/hooks/use-project";
@@ -9,6 +10,10 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/u
 import Image from "next/image";
 import { askQuestion } from "./actions";
 import { readStreamableValue } from "ai/rsc";
+import CodeReferences from './code-references';
+import { api } from '@/trpc/react';
+import { toast } from 'sonner';
+import useRefetch from '@/hooks/use-refetch';
 
 const AskQuestionCard = () => {
     const { project } = useProject();
@@ -17,14 +22,18 @@ const AskQuestionCard = () => {
     const [ loading, setLoading ] = useState(false);
     const [ filesReferences, setFileReferences ] = useState<{ fileName: string; sourceCode: string; summary: string }[]>([]);
     const [ answer, setAnswer ] = useState('');
+    const saveAnswer = api.project.saveAnswer.useMutation();
 
     const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        setAnswer("");
+        setFileReferences([]);
+
         e.preventDefault();
         if (!project?.id) return;
         setLoading(true);
-        setOpen(true);
 
         const { output, filesReference } = await askQuestion(question, project.id);
+        setOpen(true);
         setFileReferences(filesReference);
 
         for await (const delta of readStreamableValue(output)) {
@@ -36,22 +45,46 @@ const AskQuestionCard = () => {
         setLoading(false);
     }
 
+    const refetch = useRefetch();
+
     return (
         <>
 
             <Dialog open={open} onOpenChange={setOpen}>
-                <DialogContent>
+                <DialogContent className='sm:max-w-[80vw] max-h-[95vh]' data-color-mode="light">
                     <DialogHeader>
-                        <DialogTitle>
-                            <Image src='/logo.png' alt="dionysus" width={40} height={40} />
-                        </DialogTitle>
+                        <div className='flex items-center gap-2'>
+                            <DialogTitle>
+                                <Image src='/logo.png' alt="dionysus" width={40} height={40} />
+                            </DialogTitle>
+                            <Button disabled={saveAnswer.isPending} variant={"outline"} onClick={() => {
+                                saveAnswer.mutate({
+                                    projectId: project!.id,
+                                    question,
+                                    answer,
+                                    filesReferences
+                                }, {
+                                    onSuccess: () => {
+                                        toast.success("Answer saved!");
+                                        refetch();
+                                    },
+                                    onError: () => {
+                                        toast.error("Failed to save answer!");
+                                    }
+                                })
+                            }}>
+                                Save Answer
+                            </Button>
+                        </div>
                     </DialogHeader>
 
-                    {answer}
-                    <h1>Files References</h1>
-                    {filesReferences.map(file => {
-                        return <span>{file.fileName}</span>
-                    })}
+                    <MDEditor.Markdown source={answer} className='max-w-[70vw] !h-full max-h-[30vh] overflow-scroll' />
+                    <div className="h-2"></div>
+                    <CodeReferences filesReferences={filesReferences} />
+
+                    <Button type='button' onClick={() => {setOpen(false)}}>
+                        Close
+                    </Button>
 
                 </DialogContent>
             </Dialog>
@@ -64,7 +97,7 @@ const AskQuestionCard = () => {
                     <form onSubmit={onSubmit}>
                         <Textarea placeholder="Which file should I edit to change the home page" value={question} onChange={e => setQuestion(e.target.value)} />
                         <div className="h-4"></div>
-                        <Button type="submit">
+                        <Button type="submit" disabled={loading}>
                             Ask Dionysus!
                         </Button>
                     </form>
